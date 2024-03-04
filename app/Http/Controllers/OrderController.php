a54365205273ce89b1cbe1deab53c880c7e14cfe
MatheusLFumian <matheusfumian@gmail.com>
2024-03-04 11:32:47 -0300

    Add all required endpoints

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class OrderController extends Controller
 {
@@ -12,7 +13,7 @@ class OrderController extends Controller
      */
     public function index()
     {
-        //
+        return Order::all();
     }
 
     /**
@@ -20,7 +21,24 @@ class OrderController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validation = Validator::make($request->all(), [
+            'sales_id' => 'required',
+            'total_price' => 'required|numeric',
+            'products' => 'required'
+        ]);
+        if ($validation->fails()) {
+            return response()->json($validation->errors(), 422);
+        }
+
+        $data = $validation->validated();
+        $data['products'] = json_encode($validation->validated());
+
+        $order = Order::create($data);
+        if (!$order) {
+            return response()->json(['Cannot create order'], 400);
+        }
+
+        return response()->json(['Order created.']);
     }
 
     /**
@@ -28,7 +46,7 @@ class OrderController extends Controller
      */
     public function show(Order $order)
     {
-        //
+        return response()->json($order);
     }
 
     /**
@@ -39,11 +57,26 @@ class OrderController extends Controller
         //
     }
 
+    public function addProductsToOrder(Request $request, Order $order)
+    {
+        $products = $request->toArray();
+        $order_products = json_decode($order['products'], true);
+
+        array_push($products['products'], $order_products);
+        $order['products'] = json_encode($products['products']);
+        $order->save();
+        if (!$order) {
+            return response()->json(['Failed to add products to order'], 400);    
+        }
+        return response()->json(['Products added to order']);
+    }
+
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(Order $order)
     {
-        //
+        $order->deleteOrFail();
+        return response()->json(['Order canceled']);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('/product', ProductController::class);
+Route::apiResource('/order', OrderController::class);
+Route::post('/order/{order}/add_products', [OrderController::class, 'addProductsToOrder']);
